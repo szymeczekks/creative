d3d945eba7416a79fbae3bee262cb4dce30eb70d
Szymon <szymon_minko@wp.pl>
2023-03-26 13:39:22 +0200

    added site

---
+++ app.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.querySelector('.nav-btn')
+    const navList = document.querySelector('.nav-list-mobile')
+    const navSection = document.querySelector('.section-nav')
+    const changerList = document.querySelectorAll('.changer-list li');
+    const observer = new ResizeObserver((elements, observer) => {
+        for (let el of elements) {
+            if(el.contentRect.width >= 700){
+                navList.classList.remove('active-menu')
+            }
+        }
+    });
+    const el = document.querySelector("body");
+    observer.observe(el);
+
+    function checkAndToggle() {
+        const pos = el.getBoundingClientRect(); //pobieram pozycję i rozmiar elementu
+        if(pos.top < 0){
+            navSection.classList.add('active')
+            navList.classList.add('active-list')
+        } else {
+            navSection.classList.remove('active')
+            navList.classList.remove('active-list')
+        }
+    }
+    let imgsArray = [];
+    let listData = '';
+    changerList.forEach((el) => {
+        el.addEventListener('click', () => {
+            for(let i = 0; i< imgsArray.length; i++){
+                console.log(imgsArray[i]);
+                console.log(listData);
+                console.log(i+1);
+                imgsArray[i].classList.remove(`${listData}-${i+1}`)
+                console.log(imgsArray[i]);
+            }
+            imgsArray=[];
+            listData = el.getAttribute('data-cat');
+            let x = 1;
+            const changerList = document.querySelectorAll('.changer-list li');
+            listImgs = document.querySelectorAll('.box-wrap');
+            listImgs.forEach((el) => el.classList.remove('img-change'));
+            changerList.forEach((el) => el.classList.remove('active'));
+            el.classList.add('active')
+            listImgs.forEach((ell) => {
+                if(ell.getAttribute('data-cat').includes(listData)){
+                    imgsArray.push(ell);
+                    ell.classList.add('img-change');
+                    ell.classList.add(`${listData}-${x}`)
+                    x++;
+                }
+            })
+        })
+    })
+    window.addEventListener("scroll", checkAndToggle);
+    menuBtn.addEventListener('click', () => {
+        navList.classList.toggle('active-menu')
+    })
+})
